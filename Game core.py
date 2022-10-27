089770dce0523f61c01f74684c16a92c58e7edb6
Lewandodev <mlewandowski380@gmail.com>
2022-10-27 23:39:05 +0200

    Uploading rest of my sprites (for now) and making the main loop Players ship won't be uploaded yet as I'm testing if it will work correctly

--- Game core.py
+++ Game core.py
@@ -1,2 +1,42 @@
 #Core of the game with all the classes, methods, functions and my commentary
 
+import pygame
+
+
+#game window
+width=900
+height=900
+window=pygame.display.set_mode((width,height))
+#games name caption:
+pygame.display.set_caption('Python space shooter game')
+
+
+#loading assets
+red_enemy=pygame.image.load('px_ship_red_small_enemy.png')
+green_enemy=pygame.image.load('px_ship_green_small_enemy.png')
+blue_enemy=pygame.image.load('px_ship_blue_enemy_large.png')
+
+#players ship
+players_spaceship=pygame.image.load('px_spaceship_player.png')
+
+#projectiles
+
+red_laser=pygame.image.load('pixel_laser_red_proj.png')
+green_laser=pygame.image.load('pixel_laser_green_proj.png')
+blue_laser=pygame.image.load('pixel_blue_laser_proj.png')
+yellow_laser=pygame.image.load('pixel_laser_yellow_proj.png')
+
+
+#sectors backgrounds:
+background1=pygame.image.load('backgorund_sector1.png')
+
+
+def core():
+    run=True
+    FPS=60
+    #checking if player moves "x" number of Frames per second
+    clock_check=pygame.time.Clock()
+
+
+    while run:
+        clock_check.tick(FPS) #our game will stay consistent in terms of run speed
--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Game project in python inspired by the "Space Invaders" game
 
 
 This is my another game project. This one is inspired by the old aracade Shoot 'em up up games.
+Almost all of the spaceships sprites have been done by me in GIMP. Backgorunds are a collage made of different free asset pxiel space backgrounds.
+I used some of the free backgrounds that could be found on https://wallhere.com/ such as: https://wallhere.com/en/wallpaper/106712
+The main player ship is a retexture (with few additions done by me) of the free 8bit spaceship sprite from: https://www.nicepng.com/
